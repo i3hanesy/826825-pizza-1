7804c87ba5ffffd9ff801161353fe93fbe804ada
i3hanesy <i3hanesy@yandex.ru>
2022-05-23 01:41:22 +0500

    заканчивает задание Маршрутизатор и лейауты

---
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <router-link to="/" class="logo">
+        <img
+          src="@/assets/img/logo.svg"
+          alt="V!U!E! Pizza logo"
+          width="90"
+          height="40"
+        />
+      </router-link>
+    </div>
+    <div class="header__cart">
+      <router-link to="/cart"> 0 ₽ </router-link>
+    </div>
+    <div class="header__user" v-if="isLogin">
+      <router-link to="/profile">
+        <picture>
+          <source
+            type="image/webp"
+            srcset="
+              @/assets/img/users/user5.webp    1x,
+              @/assets/img/users/user5@2x.webp 2x
+            "
+          />
+          <img
+            src="img/users/user5.jpg"
+            srcset="img/users/user5@2x.jpg"
+            alt="Василий Ложкин"
+            width="32"
+            height="32"
+          />
+        </picture>
+        <span>Василий Ложкин</span>
+      </router-link>
+      <router-link to="/" class="header__logout">
+        <span>Выйти</span>
+      </router-link>
+    </div>
+    <div class="header__user" v-else>
+      <router-link to="/login" class="header__login">
+        <span>Войти</span>
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppLayoutHeader",
+  props: {
+    isLogin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header__user {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/frontend/src/layouts/AppLayoutNoHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-layout-noHeader">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLayoutNoHeader",
+};
+</script>
+
+<style lang="scss" scoped></style>
